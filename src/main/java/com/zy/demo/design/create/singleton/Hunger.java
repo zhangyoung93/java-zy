b1a672b6eb65fe1ef5fab0f416e01f506d77c886
zhangyoung93 <68599345+zhangyoung93@users.noreply.github.com>
2026-03-03 16:48:25 +0800

    feat(Serializable): add readResolve demo

--- src/main/java/com/zy/demo/design/create/singleton/Hunger.java
+++ src/main/java/com/zy/demo/design/create/singleton/Hunger.java
@@ -1,11 +1,16 @@
 package com.zy.demo.design.create.singleton;
 
+import java.io.ObjectStreamException;
+import java.io.Serializable;
+
 /**
  * 饿汉式
  *
  * @author zy
  */
-public class Hunger {
+public class Hunger implements Serializable {
+
+    private static final long serialVersionUID = 1L;
 
     private static final Hunger HUNGER = new Hunger();
 
@@ -13,6 +18,16 @@ public class Hunger {
     }
 
     public static Hunger getInstance() {
+        return HUNGER;
+    }
+
+    /**
+     * 防止反序列化破坏单例
+     *
+     * @return 单例
+     * @throws ObjectStreamException ObjectStreamException
+     */
+    private Object readResolve() throws ObjectStreamException {
         return HUNGER;
     }
 }
